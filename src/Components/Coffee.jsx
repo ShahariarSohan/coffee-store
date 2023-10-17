6c87f806dcd19e90e106a95bce4877ce1c614001
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-10-17 16:50:56 +0600

    I have added all coffee card in coffes page

--- src/Components/Coffee.jsx
+++ src/Components/Coffee.jsx
@@ -1,5 +1,38 @@
-const Coffee = () => {
-  return <div></div>;
-};
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { FaRegEye, FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+const Coffee = ({ coffee }) => {
+  const { name, chef, taste, photo } = coffee || {};
 
+  return (
+    <div className=" bg-base-200 flex gap-5  items-center mx-auto pr-10">
+      <div>
+        <img className="" src={photo} alt="" />
+      </div>
+      <div className="font-semibold">
+        <p>Name :{name}</p>
+        <p>Chef :{chef}</p>
+        <p>Taste :{taste}</p>
+      </div>
+      <div className="space-y-5">
+        <Link>
+          <button>
+            <FaRegEye className="text-2xl"></FaRegEye>
+          </button>
+        </Link>
+        <Link>
+          <FaEdit className="text-2xl"></FaEdit>
+          <button></button>
+        </Link>
+        <button>
+          <AiFillDelete className="text-2xl"></AiFillDelete>
+        </button>
+      </div>
+    </div>
+  );
+};
+Coffee.propTypes = {
+  coffee: PropTypes.object.isRequired,
+};
 export default Coffee;
--- src/Pages/Coffees.jsx
+++ src/Pages/Coffees.jsx
@@ -3,9 +3,8 @@ import Coffee from "../Components/Coffee";
 
 const Coffees = () => {
   const coffees = useLoaderData();
-  console.log(coffees);
   return (
-    <div>
+    <div className="mb-10">
       <h1 className="text-2xl text-center font-bold mb-10">Coffees</h1>
       <div className="grid grid-cols-2 gap-5">
         {coffees?.map((coffee) => (
